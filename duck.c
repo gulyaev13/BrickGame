658f6d3be8c69a54e95bca94ee5d47dd98f2a287
gulyaev13 <gulyaev13@gmail.com>
2017-01-20 11:38:02 +0700

    Add files via upload
    Add easter egg

--- duck.c
+++ duck.c
@@ -1,9 +1,42 @@
 #include "app_window.h"
 #include "SDL2/SDL.h"
 #include <stdlib.h>
+const int easter_image[200] = {
+	1, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+	1, 0, 0, 0, 1, 0, 0, 1, 0, 0,
+	1, 0, 0, 0, 1, 0, 0, 1, 0, 0,
+	1, 1, 1, 1, 0, 0, 0, 1, 0, 0,
+	1, 0, 0, 0, 1, 0, 0, 1, 0, 0,
+	1, 0, 0, 0, 1, 0, 0, 1, 0, 0,
+	1, 1, 1, 1, 0, 0, 0, 1, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+	0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	1, 1, 1, 1, 0, 0, 1, 0, 0, 1,
+	1, 0, 0, 0, 0, 0, 1, 0, 1, 1,
+	1, 0, 0, 0, 0, 0, 1, 1, 0, 1,
+	1, 1, 1, 1, 0, 0, 1, 0, 0, 1,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 1, 1, 0, 0, 0, 0, 1, 1, 0,
+	1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+	1, 0, 0, 1, 0, 0, 1, 1, 1, 1,
+	1, 0, 0, 1, 0, 0, 1, 0, 0, 1
+};
+
+static void set_easter_egg(int* const area) {
+	size_t row, colomn;
+	for (row = 0; row < PLAYGROUND_ROWS; ++row) {
+		for (colomn = 0; colomn < PLAYGROUND_COLOMNS; ++colomn) {
+			area[row * PLAYGROUND_COLOMNS + colomn] = easter_image[row * PLAYGROUND_COLOMNS + colomn];
+		}
+	}
+
+}
 int init_duck(int** area, int** info_blocs) {
 	*area = (int*)calloc(PLAYGROUND_COLOMNS * PLAYGROUND_ROWS, sizeof(int));
 	*info_blocs = (int*)calloc(INFO_BLOCKS_COUNT * INFO_BLOCKS_COUNT, sizeof(int));
+	set_easter_egg(*area);
 	return 0;
 }
 
